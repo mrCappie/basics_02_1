8c595c80dcd840d8eecc6d68d026c2154a61c1e8
Serrgey Litovchenko <e4sybreezy@gmail.com>
2024-10-26 18:47:44 +0600

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import kotlin.math.max
 
 fun main() {
     val amount = 100
-    val percent  = 0.75
+    val percent = 0.75
     val minCommission = 35
 
     val commission = max(minCommission.toDouble(), amount / 100 * percent)
