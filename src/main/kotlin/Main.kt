b3391aa96c1684241d2b840be5542446429c29c8
Serrgey Litovchenko <e4sybreezy@gmail.com>
2024-10-26 18:09:16 +0600

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,6 @@ package org.example
 
 import kotlin.math.max
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val amount = 100
     val percent  = 0.75
